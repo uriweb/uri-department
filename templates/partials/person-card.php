820184791749ef02a2369aca0de1b55085ca8022
John Pennypacker <jpennypacker@uri.edu>
2016-11-07 11:20:51 -0500

    added hcard attributes to people tool

--- templates/partials/person-card.php
+++ templates/partials/person-card.php
@@ -1,19 +1,35 @@
-<div class="peopleitem<?php if ($i % 2 == 0) { ?> endperson<?php } ?>">
+<?php
+
+	// put together a string of miscellaneous custom fields
+	$misc = array();
+	
+	if(get_field('peoplephone')) {
+		$misc[] = '<span class="p-tel">' . get_field('peoplephone') . '</span>';
+	}
+	if(get_field('peopleemail')) {
+		$misc[] = '<a href="mailto:' . get_field('peopleemail') . '" class="u-email">' . get_field('peopleemail') . '</a>';
+	}
+	
+	$misc = implode( ' &ndash; ', $misc );
+	
+?><div class="peopleitem h-card">
 	<div class="header">
-		<h3><a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+		<h3 class="p-name"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
 	</div>
 	<div class="inside">
 		<?php if ( has_post_thumbnail() ) : ?>
-		<a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail('people-thumb'); ?></a>
+		<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('people-thumb', array( 'class' => 'u-photo' )); ?></a>
 		<?php else : ?>
-		<a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/defaultsmall.gif" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" /></a>
+		<a href="<?php the_permalink() ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/defaultsmall.gif" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" /></a>
 		<?php endif; ?>
 
 		<p><?php the_field('peopletitle'); ?></p>
 		
 		<p class="people-department"><?php the_field('peopledepartment'); ?></p>
 
-		<p><?php if(get_field('peoplephone')) { ?><?php the_field('peoplephone'); ?><?php } ?><?php if(get_field('peoplephone') && get_field('peopleemail')) { ?> &ndash; <?php } ?><?php if(get_field('peopleemail')) { ?><a href="mailto:<?php the_field('peopleemail'); ?>"><?php the_field('peopleemail'); ?></a><?php } ?></p>
+		<?php if(!empty( $misc )): ?>
+			<p><?php print $misc ?></p>
+		<?php endif; ?>
 
 		<div style="clear:both;"></div>
 	</div>
