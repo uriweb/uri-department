4ae53918110289d96eb59e0f03c7115eae878d35
Brandon Fuller <bjcfuller@uri.edu>
2017-05-05 12:09:38 -0400

    A script to optionally list all CSS class and id names used on a page; useful for CSS cleaning on a page-by-page basis.  Output is on the bottom of the page.

--- js/scripts.built.js
+++ js/scripts.built.js
@@ -1,16 +1,53 @@
 // Gulp compiles scripts and puts them here.
-// Get all class and id names from page and display them in the console.
+// Get all class and id names from page and display them at the bottom of the page.
 // Helpful for CSS cleaning.
 
-jQuery(function($){
-    var classnames = [];
-    var idnames = [];
-    $('[class]').each(function(){
-        classnames.append($(this).attr('class'));
+// Run this?
+var List_CSS_Selectors = false;
+
+//===========================//
+
+if (List_CSS_Selectors) {
+    jQuery(function($){
+
+        var classes = [],
+            ids = [],
+            x;
+
+        // Function to remove duplicates
+        function dups(list) {
+            var r = [];
+            $.each(list, function(i, e) {
+                if ($.inArray(e, r) == -1) r.push(e);
+            });
+            return r.sort();
+        }
+
+        // Get unique class names
+        $('[class]').each(function(){
+            classes = classes.concat($(this).attr('class').split(' '));
+        });
+        classes = dups(classes);
+
+        // Get id names
+        $('[id]').each(function(){
+            ids.push($(this).attr('id'));
+        });
+        ids = dups(ids);
+
+        // Built output and display
+        var html = '<!-- DISPLAY CLASSES AND IDs FOR CSS CLEANING --><div style="width: 1000px; margin: 0 auto"><b>Unique Class Names:</b><br /><ul>';
+        for (x in classes) {
+            html += '<li>' + classes[x] + '</li>';
+        }
+        html += '</ul><br /><b>Unique ID Names:</b><br /><ul>';
+        for (x in ids) {
+            html += '<li>' + ids[x] + '</li>';
+        }
+        html += '</ul></div>';
+        $('#footer').after(html);
+
     });
-    $('[id]').each(function(){
-        idnames.append($(this).attr('id'));
-    });
-    console.log("Classes: " + classnames);
-    console.log("IDs: " + idnames);
-});
+}
+
+//===========================//
