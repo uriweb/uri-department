a24bfe06cb31d305b9b44b488e6fa375e3bf65bc
Brandon Fuller <bjcfuller@uri.edu>
2017-05-12 16:33:32 -0400

    Added new styles for program finder archives

---
+++ templates/partials/post-archive-modern.php
@@ -0,0 +1,17 @@
+<div class="archiveitem-modern">
+
+	<?php if ( has_post_thumbnail() ) { ?>
+		<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('people-thumb'); ?></a>
+	<?php } else { ?>
+		<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/default/uri80.gif" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" /></a>
+	<?php } ?>
+
+	<h2><?php the_title(); ?></h2>
+
+	<?php the_excerpt(); ?>
+
+	<p style="margin-top: 10px;"><?php the_tags(); ?></p>
+    
+    <a class="button" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">Learn more</a>
+
+</div>
