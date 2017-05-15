af3ae496fa2e0008feea2866b77e74aeb5010fe4
Brandon Fuller <bjcfuller@uri.edu>
2017-05-15 11:26:33 -0400

    Updated component library (new file name)

--- functions.php
+++ functions.php
@@ -209,7 +209,7 @@ function uri_department_styles() {
 	wp_register_style( 'reset', get_template_directory_uri() . '/css/reset.css', array(), $version, 'all' );
 	wp_register_style( 'thegrid', get_template_directory_uri() . '/css/grid.css', array(), $version, 'all' );
 	wp_register_style( 'basestyle', get_template_directory_uri() . '/style.css', array(), $version, 'all' );
-    wp_register_style( 'complib', get_template_directory_uri() . '/cl/style.css', array(), $version, 'all' );
+    wp_register_style( 'complib', get_template_directory_uri() . '/cl/cl.built.css', array(), $version, 'all' );
 
 	// enqueing:
 	wp_enqueue_style( 'reset' );
