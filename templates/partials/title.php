5550df85c5af47fb68675efd3a6a756306bd7982
John Pennypacker <jpennypacker@uri.edu>
2017-11-08 12:10:28 -0500

    Fixed page title bug so most pages don’t say “archives”

--- templates/partials/title.php
+++ templates/partials/title.php
@@ -1,15 +1,20 @@
 <?php
-	$onoroff = get_field('pagetitle');
-	if (of_get_option('urid_pageover') == true && !isset($onoroff)) {
-	} else {
-		if (get_field('pagetitle') == true) {
-		} else {
-			$the_title = get_the_archive_title();
-			?>
-			<div class="title">
-				<h1><?php echo str_replace('Category: ', '', $the_title); ?></h1>
-			</div>
-			<?php
-		}
+	// initialize a variable to suppress the title display
+	$suppress_title = FALSE;
+	if ( get_field('pagetitle') === TRUE ) {
+		$suppress_title = TRUE;
 	}
-?>
+	
+
+	// if the title isn't suppressed, determine its value, and print it
+	if ( !$suppress_title ) {
+
+		$headline = get_the_title();
+		// what if it's an archive page?
+		if ( is_archive() ) {
+			$headline = str_replace( 'Category: ', '', get_the_archive_title() );
+		}
+		
+		echo '<div class="title"><h1>' . $headline . '</h1></div>';
+
+	}
